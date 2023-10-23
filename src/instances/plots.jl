ec7175709880c26025e55c0df59980dc41064c3b
ajzia <jokul502@wp.pl>
2023-10-23 21:38:44 +0200

    Plot limit changes

--- src/instances/plots.jl
+++ src/instances/plots.jl
@@ -24,19 +24,25 @@ module VRPTWPlots
     :brown, :darkturquoise, :slategray, :skyblue2
   ]
 
-  
+
   function plot_routes(
     path::String,
   )::Nothing
+    println("Generating plots...")
     (data::Dict, instance_info::Vector) = get_instance_info(path, "routes")
 
     coords::Vector{Tuple{Int, Int}} =
       [(x,y) for (x, y) in data["coordinates"]]
 
-    max_x::Int = maximum([x for (x, y) in coords])
-    max_y::Int = maximum([y for (x, y) in coords])
+    max_x::Int = maximum([x for (x, y) in coords]) + 1
+    max_x = max_x + (10 - max_x % 10)
+    max_y::Int = maximum([y for (x, y) in coords]) + 1
+    max_y = max_y + (10 - max_y % 10)
 
     for algorithm in ["greedy", "population"]
+      if isempty(data[algorithm]["routes"])
+        continue
+      end
       # Depot's coordinates
       p = plot(
         coords[1],
@@ -44,8 +50,8 @@ module VRPTWPlots
         markersize=10,
         markershape=:star5,
         label="Depot",
-        xticks=0:10:100,yticks=0:10:100,
-        xlim=(0, 100), ylim = (0, 100),
+        xticks=0:10:min(max_x, 100),yticks=0:10:min(max_y, 100),
+        xlim=(0, min(max_x, 100)), ylim = (0, min(max_y, 100)),
         size=(800, 800),
         legend=:outerbottom, legendcolumns=3,
         color=:black
@@ -73,23 +79,26 @@ module VRPTWPlots
           GR.setarrowsize(0.5),
           arrow=Plots.Arrow(:closed, :head, 0.1, 0.1),
           linewidth=1,
-          title="Routes for $(instance_info[1]) with $(instance_info[2]) \
-                 customers using $(algorithm) algorithm",
+          title="$(instance_info[1]) with $(instance_info[2]) customers \
+          using $(algorithm) algorithm, \
+          cost=$(trunc(data[algorithm]["cost"], digits=3))",
           label="",
           color=color_palette[i],
         )
-        
         plot!([], [], label="Route $i", color=color_palette[i])
       end
 
       save_path::String = split(path, "/")[end]
       save_path = save_path[1:end-5]
 
+      println("> Saving plot in $(save_path)_$(algorithm).svg")
+
       dir::String = joinpath(@__DIR__, "../../results/plots/")
       (!isdir(dir)) && mkdir(dir)
       savefig(p, dir * "$(save_path)_$(algorithm).svg")
-    end
+    end # for
   end # plot_routes
+
 
   function get_instance_info(
     path::String,
@@ -102,7 +111,7 @@ module VRPTWPlots
 
     dir = joinpath(@__DIR__, dir * path)
     if !isfile(dir)
-      throw(ArgumentError("File $path does not exist")) 
+      throw("File $path does not exist")
     end
 
     data::Dict = JSON.parsefile(
@@ -112,7 +121,7 @@ module VRPTWPlots
 
     parameters::Vector{String} = split(path, "-")
     if length(parameters) < 4
-      throw(ArgumentError("File $path does not contain instance parameters"))
+      throw("File $path does not contain instance parameters")
     end
 
     instance_info = [
