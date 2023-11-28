3a790754f32f9f7423c4fd7493cf650913629afa
ajzia <jokul502@wp.pl>
2023-11-28 15:25:37 +0100

    Selection methods

---
+++ src/algorithms/ant_colony/selection.jl
@@ -0,0 +1,27 @@
+function roulette_wheel(
+  probabilities::Vector{Float64},
+)::Int
+  r::Float64 = rand()
+  prob::Float64 = 0.0
+  for (idx, p) in enumerate(probabilities)
+    prob += p
+    if r <= prob
+      return idx
+    end
+  end
+end
+
+
+# roulette wheel selection with stochastic acceptance
+# http://www.sciencedirect.com/science/article/pii/S0378437111009010
+function stochastic_acceptance(
+  probabilities::Vector{Float64},
+)::Int
+  max::Float64 = maximum(probabilities)
+  while true
+    idx::Int = rand(1:length(probabilities))
+    if rand() < probabilities[idx] / max
+      return idx
+    end
+  end
+end
